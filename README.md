9182904f233a0207dbb8d3120d5f2e3acaa8f543
София Логачева <s.logacheva@NewRNS.local>
2023-12-12 18:35:18 +0300

    логин и юзерс

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # FinalProject
 
 Запуск сервера: npm start
+
+## проверка доступности сервера через cmd:
+curl -G -d "name=admin" -d "email=admin@mail.comь" http://localhost:5000/login
--- server/index.js
+++ server/index.js
@@ -40,5 +40,19 @@ app.get('/users', (request, response) => {
     });
 });
 
+app.get('/login',(request, response)=>{
+    console.log(request)
+    console.log(`SELECT * FROM users where name="${request.query.name}" and email="${request.query.email}"`)
+    pool.query(`SELECT * FROM users where name="${request.query.name}" and email="${request.query.email}"`, (error, result) => {
+        if (error) throw error;
+        if (result.length>0) response.send(result);
+        else{
+            response.send({id: null, name: null, email: null})
+        }
+        console.log(result)
+        
+    });
+} )
+
 console.log('Сервер стартовал!');
 
